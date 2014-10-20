342dd9cbd695aa16f3b8e7dfc448eac4330a039e
Faerbit <faerbit@gmail.com>
2014-10-20 18:43:46 +0200

    Adding entity.hh

---
+++ entity.hh
@@ -0,0 +1,19 @@
+#ifndef ENTITIY_HH_INCLUDED
+#define ENTITY_HH_INCLUDED
+
+#include <ACGL/Math/Math.hh>
+
+#endif
+
+class Entity {
+    public:
+        Entity(glm::vec3 position, glm::vec3 rotation);
+        ~Entity();
+        void setPosition(glm::vec3 positon);
+        void setRotation(glm::vec3 rotation);
+        glm::vec3 getPosition();
+        glm::vec3 getRotation();
+    private:
+        glm::vec3 position;
+        glm::vec3 rotation;
+};
