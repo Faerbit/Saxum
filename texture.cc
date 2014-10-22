2d3a17b6c029c0b6dfbea3654ce4bef842d2203d
Faerbit <faerbit@gmail.com>
2014-10-22 23:17:18 +0200

    Implemented texture.cc

---
+++ texture.cc
@@ -0,0 +1,12 @@
+#include "texture.hh"
+
+Texture::Texture(std::string filePath) {
+    reference = ACGL::OpenGL::Texture2DFileManager::the()->get(ACGL::OpenGL::Texture2DCreator(filePath));
+}
+
+Texture::~Texture() {
+}
+
+ACGL::OpenGL::SharedTexture2D Texture::getReference() {
+    return reference;
+}
--- texture.hh
+++ texture.hh
@@ -2,14 +2,17 @@
 #define TEXTURE_HH_INCLUDED
 
 #include <string>
+#include <ACGL/OpenGL/Creator/Texture2DCreator.hh>
+#include <ACGL/OpenGL/Data/TextureLoadStore.hh>
+#include <ACGL/OpenGL/Managers.hh>
 
 class Texture{
     public:
         Texture(std::string filePath);
+        ACGL::OpenGL::SharedTexture2D getReference();
         ~Texture();
-        void load();
     private:
-        std::string filePath;
+        ACGL::OpenGL::SharedTexture2D reference;
 };
 
 #endif
