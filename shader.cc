1d924bd2b05f1af85b02a0b622fa37b47a1477c2
Faerbit <faerbit@gmail.com>
2014-10-23 00:52:04 +0200

    Adding shader class.

---
+++ shader.cc
@@ -0,0 +1,14 @@
+#include "shader.hh"
+
+Shader::Shader(std::string filePath, Model model) {
+    reference = ACGL::OpenGL::ShaderProgramCreator(filePath).attributeLocations(
+            model.getReference()->getAttributeLocations()).create();
+    reference->use();
+}
+
+Shader::~Shader() {
+}
+
+ACGL::OpenGL::SharedShaderProgram Shader::getReference() {
+    return reference;
+}
---
+++ shader.hh
@@ -0,0 +1,17 @@
+#ifndef SHADER_HH_INCLUDED
+#define SHADER_HH_INCLUDED
+
+#include "model.hh"
+#include <string>
+#include <ACGL/OpenGL/Creator/ShaderProgramCreator.hh>
+
+class Shader {
+    public:
+        Shader(std::string filePath, Model model);
+        ACGL::OpenGL::SharedShaderProgram getReference();
+        ~Shader();
+    private:
+        ACGL::OpenGL::SharedShaderProgram reference;
+};
+
+#endif
