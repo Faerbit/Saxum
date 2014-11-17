f87af894c1effe9816139bde15929ab3fc91c3b0
Fabian Klemp <fabian.klemp1@rwth-aachen.de>
2014-11-17 16:15:42 +0100

    Added alpha shading.

--- main.cc
+++ main.cc
@@ -112,6 +112,8 @@ int main( int argc, char *argv[] )
     //
     glClearColor( 0.0, 0.0, 0.0, 1.0 );
     glEnable( GL_DEPTH_TEST );
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     app.init();
 
     int frameCount = 0;
