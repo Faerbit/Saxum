3a9a04d4d6e2e41468b94b59ccb2a4c5a20136e1
Faerbit <faerbit@gmail.com>
2014-11-22 00:39:58 +0100

    Changed skybox to skydome.

--- main.cc
+++ main.cc
@@ -17,7 +17,7 @@
 #include <ACGL/Base/Settings.hh>
 
 Application::Application() {
-    graphics = Graphics(glm::uvec2(1024, 786), 0.1f, 100.0f);
+    graphics = Graphics(glm::uvec2(1024, 786), 0.1f, 150.0f);
 }
 
 Graphics* Application::getGraphics() {
@@ -35,7 +35,7 @@ ACGL::OpenGL::SharedShaderProgram Application::getShader() {
 void Application::init()
 {
     // set Skybox size
-    level.setSkyboxSize((graphics.getFarPlane()-32.0f)/sqrt(2));
+    level.setSkydomeSize((graphics.getFarPlane()/2.0f)-10.0f);
 
     // define where shaders and textures can be found:
     ACGL::Base::Settings::the()->setResourcePath("../");
