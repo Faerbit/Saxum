c4ed64fecc47545fc820910bdb10b544f778cdad
Faerbit <faerbit@gmail.com>
2014-11-24 02:10:25 +0100

    Enabling cross compiling if supported by the current system.

--- build.sh
+++ build.sh
@@ -8,6 +8,13 @@ then
     exit
 fi
 
+cmake="cmake"
+
+if [[ $1 == "windows" ]]
+then
+    cmake=x86_64-w64-mingw32-cmake
+fi
+
 currentDir=$(pwd)
 threads=$(($(nproc)+1))
 
@@ -16,7 +23,7 @@ threads=$(($(nproc)+1))
 cd extern/bullet/
 mkdir -p build
 cd build
-cmake -DBUILD_DEMOS=0 -DBUILD_EXTRAS=0 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0 ..
+$cmake -DBUILD_DEMOS=0 -DBUILD_EXTRAS=0 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0 ..
 make -j$threads
 
 cd $currentDir
@@ -25,5 +32,5 @@ cd $currentDir
 
 mkdir -p build
 cd build 
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0 ..
+$cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0 ..
 make -j$threads
