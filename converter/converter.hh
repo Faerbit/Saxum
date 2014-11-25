3de10e73aed10dc6126bccf16988ae51286d222f
Steffen Fündgens <steffen.fuendgens@rwth-aachen.de>
2014-11-25 13:51:25 +0100

    skeleton runs now, only xml read/write missing

--- converter/converter.hh
+++ converter/converter.hh
@@ -1,17 +1,27 @@
 #ifndef CONVERTER_INCLUDED
 #define CONVERTER_INCLUDED
+
 #include <vector>
+#include <string>
 #include "tinyxml2.hh"
 
+using namespace tinyxml2;
 class Converter {
     public:
+        Converter(std::string level);
         Converter();
         ~Converter();
-        void updateComposition(unsigned int type, unsigned int idG, unsigned int idB, unsigned int posX, unsigned int posZ);
+        void updateComposition(unsigned int idG, unsigned int idB, unsigned int posX, unsigned int posZ);
         std::vector<unsigned int> newComposition(unsigned int type, unsigned int posX, unsigned int posZ);
 	    void deleteComposition(unsigned int idG, unsigned int idB);
+	    void save();
+	    std::vector<unsigned int> getNextId();
 
     private:
+        std::vector<unsigned int> nextId;
+        std::string xmlFile;
+        XMLDocument* doc = new XMLDocument();
         void newObject(unsigned int type, unsigned int idG, unsigned int idB, unsigned int posX, unsigned int posZ);
 };
+
 #endif
--- converter/main.cc
+++ converter/main.cc
@@ -4,26 +4,25 @@
 #include <vector>
 #include <iostream>
 
-int main( int argc, char *argv[] ){  
-    unsigned int level=atoi(argv[1]);
-    Converter conv;
+int main( int argc, char *argv[] ){
+    std::string levelString = argv[1];
+    Converter conv = Converter(levelString);
     bool idFound[256][256];
     for (int i=0; i<256; i++){
         for (int j=0; j<256; j++){
             idFound[i][j] = false;
         }
     }
-    //read the setup png
-    char levelChar[2];
-    sprintf (levelChar,"%i", level);
-    std::string levelString = levelChar;
-    std::string filePath = "../Levels/ObjectSetups/Lvl" + levelString + ".png";
+    
+    //read the setup png    
+    std::string filePath = "../Levels/ObjectSetups/Level" + levelString + ".png";
     std::vector<unsigned char> image; //the raw pixels
     unsigned int width, height;
     unsigned error = lodepng::decode(image, width, height, filePath);
     if (error) {
         std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
     }
+    
     //iterate over all pixels of the image
     for(unsigned int rowNum = 0; rowNum < height; rowNum++){
         for(unsigned int columnNum = 0; columnNum < width; columnNum++){
@@ -37,25 +36,30 @@ int main( int argc, char *argv[] ){
                     image[pixel+1] = temp[0];
                     image[pixel+2] = temp[1];
                 }else{
-                    conv.updateComposition(image[pixel], image[pixel+1], image[pixel+2], rowNum, columnNum);
+                    conv.updateComposition(image[pixel+1], image[pixel+2], rowNum, columnNum);
                     idFound[image[pixel+1]][image[pixel+2]] = true;
                 }
             }
         }
     }
+    
     //write ids back to the setup png
     error = lodepng::encode(filePath, image, width, height);
     if(error) {
         std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
     }
+    
     //delete compositions that were not in the png anymore
-    for (int i=0; i<256; i++){
-        for (int j=0; j<256; j++){
-            if (idFound[i][j] == false){
-                //TODO if (exists)?
+    
+    for (int i=1; i<=conv.getNextId()[1]; i++){
+        for (int j=1; j<256; j++){
+            if (! idFound[i][j]){
                 conv.deleteComposition(i,j);
             }
         }
     }
+    
+    //save the xml
+    conv.save();
 }
 
