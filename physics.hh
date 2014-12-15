e49e4c21775048dc0e3186818613d916ff181010
Jasper <Jaspermanousek@hotmail.com>
2014-12-15 16:02:04 +0100

    Composition scales addapted Physics added trimeshshapes

--- physics.hh
+++ physics.hh
@@ -4,6 +4,8 @@
 #include <ACGL/Base/Settings.hh>
 #include <ACGL/Math/Math.hh>
 #include <vector>
+#include <string>
+#include <stdio.h>
 
 #include "entity.hh"
 
@@ -17,6 +19,8 @@
 #include "extern/bullet/src/BulletCollision/CollisionShapes/btStaticPlaneShape.h"
 #include "extern/bullet/src/BulletCollision/CollisionShapes/btBvhTriangleMeshShape.h"
 #include "extern/bullet/src/BulletCollision/CollisionShapes/btTriangleMesh.h"
+#include "extern/bullet/src/BulletCollision/CollisionShapes/btCollisionShape.h"
+#include "extern/bullet/src/BulletCollision/CollisionShapes/btConvexTriangleMeshShape.h"
 
 #include "extern/bullet/src/BulletDynamics/ConstraintSolver/btConstraintSolver.h"
 #include "extern/bullet/src/BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolver.h"//YAY!
@@ -51,20 +55,21 @@ class Physics {
 	void addStaticGroundPlane();
 	void addCamera(float rad,float distance); //Do NOT impliment before Player has been created;
 	glm::vec3 getCameraPosition();
-    void addTriangleMeshBody(Entity entity, float mass, float dampningL, float dampningA,unsigned indice);
+	void addRigidBodyFromFile(Entity entity, float mass, float dampningL, float dampningA, std::string modelLocation,unsigned indice);
+    void addTriangleMeshBody(Entity entity, std::string path, float mass, float dampningL, float dampningA,unsigned indice);
 	void addTerrain(int width, int length, float** heightData);
+    void addTerrainTriangles(int width, int length, float** heightData); //add the terrain as a trimesh instead of a heightmap
 	void addPlayer(float rad, Entity entity, float mass, unsigned indice); //use these AFTER physicObjects.push_back(object)! if mass == 0 then the object is unmoveable
 	void addSphere(float rad, Entity entity, float mass, unsigned indice); //The Indice should be set to physicObjects.size()
 	void addBox(float width, float height, float length, Entity entity, float mass, unsigned indice); //this is used to ensuer that the system is synchronized
 
     private:
-	btRigidBody* playerBall;    //allows for quicker access to the ball
+	btRigidBody* playerBall;    //allows for easier access to the ball
 	btRigidBody* terrainBody;   //duh
 	btRigidBody* cameraBody;
 	std::vector<btRigidBody*> bodies; //list of all bodies. bodies are also in world, but save again to ease cleaning up process.
         btRigidBody* staticGroundBody;
-
-
+    
 	btDynamicsWorld* world; //contains physical attributes of the world.
 	btDispatcher* dispatcher; //
 	btCollisionConfiguration* colConfig; //defines the type of collision detection.
@@ -72,6 +77,12 @@ class Physics {
 	btConstraintSolver* solver; //solver for forces and impulses.
 };
 
+enum collisionTypes{
+    COL_NOTHING = 0,
+    COL_TERRAIN = 1,
+    COL_OBJECTS = 2,
+    COL_OBJECTS_NO_TERRAIN = 4
+};
 
 class btDistanceConstraint : public btPoint2PointConstraint
 {
