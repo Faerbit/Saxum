3450abd9707015738ceb9b21ede5769e8645ae7c
Steffen Fündgens <steffen.fuendgens@rwth-aachen.de>
2014-12-15 13:36:04 +0100

    Startet implementing a trigger skeleton.

---
+++ trigger.cc
@@ -0,0 +1,11 @@
+#include "trigger.hh"
+
+Trigger::Trigger(std::vec<float> position, float distance, bool isBigger, objectIndex, int functionPointer) {
+
+}
+
+Trigger::Trigger(){
+}
+
+Trigger::~Trigger(){
+}
