81bead5a97d1de23f62136aabda15540fba4f544
Steffen Fündgens <steffen.fuendgens@rwth-aachen.de>
2014-12-15 13:39:24 +0100

    Fixed the trigger skeleton

--- trigger.cc
+++ trigger.cc
@@ -1,7 +1,7 @@
 #include "trigger.hh"
 
-Trigger::Trigger(std::vec<float> position, float distance, bool isBigger, objectIndex, int functionPointer) {
-
+Trigger::Trigger(std::vector<float> position, float distance, bool isBigger, int objectIndex, int functionPointer) {
+    
 }
 
 Trigger::Trigger(){
