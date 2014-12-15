e531ede14405b470fc2a50192e2c0dff3377fa0a
Faerbit <faerbit@gmail.com>
2014-12-15 01:05:46 +0100

    Init graphics with the level instead of passing it to the render function everytime.

--- main.cc
+++ main.cc
@@ -77,6 +77,7 @@ int main( int argc, char *argv[] )
     glEnable( GL_DEPTH_TEST );
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
     app.init();
 
     int frameCount = 0;
@@ -123,7 +124,7 @@ int main( int argc, char *argv[] )
         }
 
         lastUpdate = now;
-        app.getGraphics()->render(app.getLevel());
+        app.getGraphics()->render();
         
         openGLCriticalError();
 
