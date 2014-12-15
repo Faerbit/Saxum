66cf9ef8d604937fb6845ec3d87a2c8a1e8c8323
Steffen Fündgens <steffen.fuendgens@rwth-aachen.de>
2014-12-15 14:59:03 +0100

    Implemented half the loading of triggers.

--- trigger.cc
+++ trigger.cc
@@ -1,11 +1,19 @@
 #include "trigger.hh"
 
-Trigger::Trigger(std::vector<float> position, float distance, bool isBigger, int objectIndex, int functionPointer) {
-    
+Trigger::Trigger(std::vector<float> position, float distance, bool isBigger, Object* object, void (*functionPointer)()) {
+    this->position=position;
+    this->distance=distance;
+    this->isBigger=isBigger;
+    this->object=object;
+    this->functionPointer=functionPointer;
 }
 
 Trigger::Trigger(){
 }
 
 Trigger::~Trigger(){
+}
+
+void Trigger::triggerUpdate(){
+
 }
