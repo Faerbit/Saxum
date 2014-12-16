be4a61e65b533b91be5662594d60905ffbfe4d74
Fabian Klemp <fabian.klemp1@rwth-aachen.de>
2014-12-16 12:19:48 +0100

    Fixed triggers. Now deletes a truly random object.

--- trigger.cc
+++ trigger.cc
@@ -1,11 +1,12 @@
 #include "trigger.hh"
 
-Trigger::Trigger(glm::vec3 position, float distance, bool isBigger, Object* object, void (*functionPointer)()) {
+Trigger::Trigger(glm::vec3 position, float distance, bool isBigger, Object* object, void (*functionPointer)(Level*), Level* level) {
     this->position=position;
     this->distance=distance;
     this->isBigger=isBigger;
     this->object=object;
-    this->functionPointer = functionPointer;   
+    this->functionPointer = functionPointer;
+    this->level = level;
 }
 
 Trigger::Trigger(){
@@ -16,9 +17,9 @@ Trigger::~Trigger(){
 
 void Trigger::triggerUpdate(){
     if (isBigger && (glm::distance(object->getPosition(), position) > distance)) {
-        (*functionPointer)();
+        (*functionPointer)(level);
     }
     else if (!isBigger && (glm::distance(object->getPosition(), position) < distance)) {
-        (*functionPointer)();
+        (*functionPointer)(level);
     }
 }
