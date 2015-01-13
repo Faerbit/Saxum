74d45e7d38a97b50605532ee49dbc31a10338839
Steffen Fündgens <steffen.fuendgens@rwth-aachen.de>
2015-01-13 17:21:15 +0100

    Lua script works now, worked around the bug by saving the name as a std::string.

--- trigger.cc
+++ trigger.cc
@@ -1,17 +1,11 @@
 #include "trigger.hh"
-#include "level.hh"
 
-Trigger::Trigger(glm::vec3 position, float distance, bool isBigger, Object* object, std::string luaScript, lua_State* L, int objectToChange, Level* level) {
+Trigger::Trigger(glm::vec3 position, float distance, bool isBigger, Object* object, std::string luaScript, lua_State* L, int objectToChange) {
     this->position=position;
     this->distance=distance;
     this->isBigger=isBigger;
     this->object=object;
-    this->triggerLuaScript=("../Levels/scripts/" + luaScript + ".lua").c_str();
-    printf("Hallo\n");
-    printf(this->triggerLuaScript);
-    printf("Hallo\n");
-    printf("\n");
-    this->level = level;
+    this->luaScript="../Levels/scripts/" + luaScript + ".lua";
     this->L = L;
     if(L == nullptr){
         printf("L is NULL in trigger!\n");
@@ -29,41 +23,14 @@ Trigger::~Trigger(){
 void Trigger::triggerUpdate(){
     if (!triggered){
         if (isBigger && (glm::distance(object->getPosition(), position) > distance)) {
-            printf(triggerLuaScript);
-            printf("\n");
-            printf(triggerLuaScript);
-            printf("\nTriggering\n");
-            //luaL_dofile(L, luaScript);
-            //luabridge::getGlobal(L, "trigger")(objectToChange);
+            luaL_dofile(L, luaScript.c_str());
+            luabridge::getGlobal(L, "trigger")(objectToChange);
             triggered = true;
         }
         else if (!isBigger && (glm::distance(object->getPosition(), position) < distance)) {
-            printf(triggerLuaScript);
-            printf("\n");
-            printf(triggerLuaScript);
-            printf("\nTriggering\n");
-            //luaL_dofile(L, luaScript);
-            //luabridge::getGlobal(L, "trigger")(objectToChange);
+            luaL_dofile(L, luaScript.c_str());
+            luabridge::getGlobal(L, "trigger")(objectToChange);
             triggered = true;
         }
     }
-}
-
-void Trigger::trigger_function_0(Level* level) {
-    if (!triggered) {
-        int rand = std::rand() % level->getObjects()->size();
-        level->getObjects()->erase(level->getObjects()->begin() + rand);
-    }
-}
-
-void Trigger::trigger_function_1(Level* level) {
-}
-
-void Trigger::trigger_function_2(Level* level) {
-}
-
-void Trigger::trigger_function_3(Level* level) {
-}
-
-void Trigger::trigger_function_4(Level* level) {
 }
