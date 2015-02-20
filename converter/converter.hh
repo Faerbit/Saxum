563bde0616ec8814a1f6ff242eb2b0d30e2730c2
Steffen Fündgens <steffen.fuendgens@rwth-aachen.de>
2015-02-20 13:11:05 +0100

    Now using xml query functions in the converter. Also adjusted the converter to use config.xml. Running the converter would delete all triggers and position constraints though, because the IDs in the png were manually overwritten with zeros.

--- converter/converter.hh
+++ converter/converter.hh
@@ -8,20 +8,25 @@
 using namespace tinyxml2;
 class Converter {
     public:
-        Converter(std::string level);
+        Converter(std::string levelPath, std::string compositionsPath);
         Converter();
         ~Converter();
         void updateComposition(int idG, int idB, float posX, float posZ); //updates the position of a composition
-        std::vector<int> newComposition(int type, float posX, float posZ);//creates a new composition and returns its ID
+        std::vector<int> newComposition(int type, float posX, float posZ); //creates a new composition and returns its ID
         void deleteComposition(int idG, int idB);
-        void save();                    //writes the xml to file
+        void save(); //writes the xml to file
     private:
-        void errorCheck(XMLError error);
+        
         std::vector<int> nextID;
         bool idUsed[256][256];
         std::string xmlFile;
         XMLDocument* doc = new XMLDocument();
         XMLDocument* compositions = new XMLDocument();
+        int queryInt(XMLElement* element, const char* attribute);
+        int queryInt(XMLDocument*& element, const char* attribute);
+        bool queryBool(XMLElement* element, const char* attribute);
+        bool queryBool(XMLDocument*& element, const char* attribute);
+        void errorCheck(XMLError error);
 };
 
 #endif
--- converter/main.cc
+++ converter/main.cc
@@ -3,15 +3,18 @@
 #include "converter.hh"
 #include <vector>
 #include <iostream>
+#include "tinyxml2.hh"
+
+using namespace tinyxml2;
 
 int main( int argc, char *argv[] ){
     if (argc <= 1){
-        std::cout << "Converter needs the level (1,2,...) as input." << std::endl;
+        std::cout << "Converter needs the levels name (Level1) as input." << std::endl;
         exit(-1);
     }
     printf("Initializing.\n");
     std::string levelString = argv[1];
-    Converter conv = Converter(levelString);
+    
     
     bool idFound[256][256];
     for (int i=0; i<256; i++){
@@ -20,14 +23,48 @@ int main( int argc, char *argv[] ){
         }
     }
     
+    XMLDocument* config = new XMLDocument();
+    const char* xmlFile = "../data/config.xml";
+    config->LoadFile(xmlFile);
+    if (config->ErrorID()!=0){
+        printf("Could not open config.xml!\n");
+        exit(-1);
+    }
+    
+    XMLElement* xmlLevelPath = config->FirstChildElement("levelXmlPath");
+    if (xmlLevelPath == NULL){
+        std::cout << "XMLError: Attribute levelXmlPath does not exist." << std::endl;
+        exit(-1);
+    }
+    const char* charLevelPath = xmlLevelPath->GetText();
+    if(charLevelPath == NULL){
+        std::cout << "XMLError: Attribute levelXmlPath could not be loaded." << std::endl;
+        exit(-1);
+    }
+    std::string levelPath = charLevelPath;
+    
+    XMLElement* xmlCompositionsPath = config->FirstChildElement("compositionsPath");
+    if (xmlCompositionsPath == NULL){
+        std::cout << "XMLError: Attribute compositionsPath does not exist." << std::endl;
+        exit(-1);
+    }
+    const char* charCompositionsPath = xmlCompositionsPath->GetText();
+    if(charCompositionsPath == NULL){
+        std::cout << "XMLError: Attribute compositionsPath could not be loaded." << std::endl;
+        exit(-1);
+    }
+    std::string compositionsPath = charCompositionsPath;
+    Converter conv = Converter("../" + levelPath + levelString, "../" + compositionsPath);
+    
     //read the setup png
     printf("Loading the png.\n");
-    std::string filePath = "../Levels/ObjectSetups/Level" + levelString + ".png";
+    std::string filePath = "../" + levelPath + levelString + ".png";
     std::vector<unsigned char> image; //the raw pixels
     unsigned int width, height;
     unsigned error = lodepng::decode(image, width, height, filePath);
     if (error) {
         std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+        std::cout << "Converter needs the levels name (Level1) as input." << std::endl;
         exit(-1);
     }
     printf("Iterating over the png.\n");
@@ -70,4 +107,3 @@ int main( int argc, char *argv[] ){
     //save the xml
     conv.save();
 }
-
