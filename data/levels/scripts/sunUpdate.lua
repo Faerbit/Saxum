251048bbd0e78d8ed7d61e13b4c7c5f8fb8a9189
Jasper <Jaspermanousek@hotmail.com>
2015-03-09 13:58:48 +0100

    modified starting sunhight + sunrise functions

--- data/levels/scripts/sunUpdate.lua
+++ data/levels/scripts/sunUpdate.lua
@@ -13,9 +13,16 @@ function trigger(objectToChange)
             timeDiff = maxTimeDiff
         end
         timeDiff = timeDiff/maxTimeDiff
-        local x = 1 - 0.3 * timeDiff
-        local y = math.sqrt(timeDiff) * 1.0 - 0.1
-        local z = 0.7 * timeDiff
+        local x = 10
+        local y = -18 + math.sqrt(timeDiff) * 29
+        local z = -8 + timeDiff * 15
         level:setSunDirection(x,y,z)
     end
 end
+
+
+        --local x = 0.7 + 0.3 * timeDiff
+        --local y = math.sqrt(timeDiff) * 2.2 - 1.5
+        --local z = -0.7 - 0.7 * timeDiff
+        --start 1,-6,-2
+        --ziel 10,10,10
