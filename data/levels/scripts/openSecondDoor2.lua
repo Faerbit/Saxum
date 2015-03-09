b4f20affebe397186219b411f79795f8d426a6ab
Fabian Klemp <fabian.klemp1@rwth-aachen.de>
2015-03-09 16:53:26 +0100

    Changed green flame parameters. They don't clip anymore.

--- data/levels/scripts/openSecondDoor2.lua
+++ data/levels/scripts/openSecondDoor2.lua
@@ -29,7 +29,7 @@ function trigger(objectToChange)
         local yPos = 25.0
         local zPos = 26.5
         level:preloadLightPosition(xPos, yPos, zPos)
-        level:addLightByParameters(0.2, 0.9, 0.0, 4.0, -2.5, 1.3, 2.3)
+        level:addLightByParameters(0.2, 0.9, 0.0, 4.0, -2.0, 1.3, 2.0)
         print("openSecondDoor2")
     end
 end
